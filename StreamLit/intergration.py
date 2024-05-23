4820c3b647efdbf364c989ec90adb06091a4f899
Lethu-hub <124559605+Lethu-hub@users.noreply.github.com>
2024-05-23 14:46:51 +0200

    Add files via upload

--- StreamLit/intergration.py
+++ StreamLit/intergration.py
@@ -1,34 +1,51 @@
-#geolocation intergration
-import requests
+import os
+import pandas as pd
 
-def get_location(ip_address):
-    url = f'http://ip-api.com/json/{ip_address}'
-    response = requests.get(url)
-    data = response.json()
-    return data
+def integrate_csv_files(input_directory, output_file):
+    # Initialize an empty list to hold the data from all files
+    all_logs = []
 
-# Example usage:
-ip_address = '148.185.153.148'
-location_data = get_location(ip_address)
-print(location_data)
+    # List all CSV files in the directory
+    csv_files = [file for file in os.listdir(input_directory) if file.endswith('.csv')]
 
-import tweepy
+    # Counter to track progress
+    log_count = 0
+    file_count = 0
+    total_files = len(csv_files)
 
-# Define your Twitter API credentials
-consumer_key = 'your_consumer_key'
-consumer_secret = 'your_consumer_secret'
-access_token = 'your_access_token'
-access_token_secret = 'your_access_token_secret'
-# Authenticate with Twitter API
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
-api = tweepy.API(auth)
+    try:
+        # Read each CSV file and append its content to the list
+        for csv_file in csv_files:
+            file_path = os.path.join(input_directory, csv_file)
+            log_df = pd.read_csv(file_path)
+            all_logs.append(log_df)
 
-# Search for mentions of your website content
-mentions = api.search(q='your_website_keyword', count=10)
+            # Update log count and file count
+            log_count += len(log_df)
+            file_count += 1
 
-# Process and analyze the mentions
-for mention in mentions:
-    print(mention.text)
-    print(mention.user.screen_name)
-    print(mention.created_at)
+            # Provide feedback every 500 logs
+            if log_count % 500 == 0:
+                print(f"Processed {log_count} logs so far...")
+
+            # Provide feedback for every file
+            print(f"Processed file {file_count}/{total_files}")
+
+        # Concatenate all DataFrames
+        all_logs_df = pd.concat(all_logs, ignore_index=True)
+
+        # Save the combined DataFrame to a single CSV file
+        all_logs_df.to_csv(output_file, index=False)
+        print(f"Integration successful. Integrated web logs saved to {output_file}")
+
+    except Exception as e:
+        print(f"Integration failed: {e}")
+
+# Directory containing the individual log files
+input_directory = 'weblogs'
+
+# Output file path
+output_file = 'synthetic_web_logs.csv'
+
+# Integrate the CSV files
+integrate_csv_files(input_directory, output_file)
